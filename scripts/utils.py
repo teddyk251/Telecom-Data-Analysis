91b29758971399aae83db247f93d36f47f6d5c27
Teddy <teddylnk1@gmail.com>
2022-05-10 11:48:20 +0300

    created a utility class for basic operations

---
+++ scripts/utils.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+
+class Utils:
+
+    def load_data(self, data_path):
+        """
+        Load data from a csv file.
+        """
+        try:
+            df = pd.read_csv(data_path)
+        except FileNotFoundError:
+            print("File not found.")
+        return df
+
+    def save_data(self, df, data_path):
+        """
+        Save data to a csv file.
+        """
+        try:
+            df.to_csv(data_path)
+        except Exception as e:
+            print(f"Saving failed {e}")
+    
+    def convert_bytes_to_mb(self, bytes):
+        """
+        Convert bytes to megabytes.
+        """
