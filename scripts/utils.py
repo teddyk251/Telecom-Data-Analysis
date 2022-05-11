250c739b8ecc7938081ee6108961f667011ab1dd
Teddy <teddylnk1@gmail.com>
2022-05-11 20:31:34 +0300

    added function annotations

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
 
+
 class Utils:
 
-    def load_data(self, data_path):
+    def load_data(self, data_path: str) -> pd.DataFrame:
         """
         Load data from a csv file.
         """
@@ -13,7 +14,7 @@ class Utils:
             print("File not found.")
         return df
 
-    def save_data(self, df, data_path):
+    def save_data(self, df:pd.Dataframe, data_path:str) -> None:
         """
         Save data to a csv file.
         """
@@ -21,5 +22,10 @@ class Utils:
             df.to_csv(data_path)
         except Exception as e:
             print(f"Saving failed {e}")
-    
 
+    def convert_bytes_to_megabytes(self, df: pd.DataFrame, bytes_data) -> pd.Series:
+
+        megabyte = 1*10e+5
+        megabyte_col = df[bytes_data] / megabyte
+
+        return megabyte_col
