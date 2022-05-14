d24d36b9f4bdfc417d1fb7766b9c4a10f7a7d7cd
Teddy <teddylnk1@gmail.com>
2022-05-14 22:50:05 +0300

    added user satisfaction page

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ sys.path.append("../dashboard/")
 
 import streamlit as st
 from multiapp import MultiApp
-from dashboard import user_overview_page, user_engagement_page, user_experience_page
+from dashboard import user_overview_page, user_engagement_page, user_experience_page, user_satisfaction_page
 
 st.set_page_config(page_title="TelCo Telecom Analytics", layout="wide")
 
@@ -20,8 +20,7 @@ st.sidebar.markdown("""
 app.add_app("User Overview Analysis", user_overview_page.app)
 app.add_app("User Engagement Analysis", user_engagement_page.app)
 app.add_app("User Experience Analysis", user_experience_page.app)
-# app.add_app("User Satisfaction Analysis", user_satisfaction_analysis_page.app)
-# app.add_app("Predict Satisfaction", model_implementation.app)
+app.add_app("User Satisfaction Analysis", user_satisfaction_page.app)
 
 # The main app
 app.run()
