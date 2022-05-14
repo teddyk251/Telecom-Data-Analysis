c32de3b2dd42104388626c313127ac7dfba07cf5
Teddy <teddylnk1@gmail.com>
2022-05-14 22:47:32 +0300

    export user satisfaction data for dashboard

--- app.py
+++ app.py
@@ -4,28 +4,22 @@ sys.path.append("../dashboard/")
 
 import streamlit as st
 from multiapp import MultiApp
-from dashboard import user_overview_page
-# , user_engagement_analysis_page, user_experience_analysis_page , user_satisfaction_analysis_page, model_implementation
-# import your app modules here
+from dashboard import user_overview_page, user_engagement_page, user_experience_page
 
-st.set_page_config(page_title="TellCo Telecom Analytics", layout="wide")
+st.set_page_config(page_title="TelCo Telecom Analytics", layout="wide")
 
 app = MultiApp()
 
 
 st.sidebar.markdown("""
-# TellCo's User Analytics
-### Multi-Page App
-This multi-page app is using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework developed by [Praneel Nihar](https://medium.com/@u.praneel.nihar). Also check out his [Medium article](https://medium.com/@u.praneel.nihar/building-multi-page-web-app-using-streamlit-7a40d55fa5b4).
-### Modifications
-\t- Page Folder Based Access
-\t- Presentation changed to SideBar
+# TelCo's User Analytics
+
 """)
 
 # Add all your application here
 app.add_app("User Overview Analysis", user_overview_page.app)
-# app.add_app("User Engagement Analysis", user_engagement_analysis_page.app)
-# app.add_app("User Experience Analysis", user_experience_analysis_page.app)
+app.add_app("User Engagement Analysis", user_engagement_page.app)
+app.add_app("User Experience Analysis", user_experience_page.app)
 # app.add_app("User Satisfaction Analysis", user_satisfaction_analysis_page.app)
 # app.add_app("Predict Satisfaction", model_implementation.app)
 
